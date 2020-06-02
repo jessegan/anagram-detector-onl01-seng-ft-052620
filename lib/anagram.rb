8bc95d0b70fdaea46586d4891ad65f8781c3ced7
jessegan <jesse13579@gmail.com>
2020-06-02 15:56:45 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,13 @@
 class Anagram
 
+    attr_accessor :word
+
     def initialize(word)
         @word = word
     end
 
     def match(anagrams)
-        sorted = @word.downcase.chars.sort
+        sorted = self.word.downcase.chars.sort
         anagrams.select do |match|
             match.downcase.chars.sort == sorted
         end
