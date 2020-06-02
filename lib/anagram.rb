9f17f64ce7bacae1bd48a89854cd79f50cd6d91f
jessegan <jesse13579@gmail.com>
2020-06-02 15:51:41 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagrams)
+        sorted = @word.downcase.chars.sort.join
+        anagrams.select do |match|
+            match.downcase.chars.sort.join == sorted
+        end
+    end
+    
+end
